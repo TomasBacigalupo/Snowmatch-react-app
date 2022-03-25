9f8e0b835548f9f28892bdfbe951eb318acfc0a8
Lucio Pagni <luciopagni@Lucios-MacBook-Air.local>
2022-03-25 02:03:45 -0300

    comment contact pop over

--- src/layouts/dashboard/header/index.js
+++ src/layouts/dashboard/header/index.js
@@ -84,7 +84,7 @@ export default function DashboardHeader({ onOpenSidebar, isCollapse = false, ver
         <Stack direction="row" alignItems="center" spacing={{ xs: 0.5, sm: 1.5 }}>
           <LanguagePopover />
           <NotificationsPopover />
-          <ContactsPopover />
+          {/* <ContactsPopover /> */}
           <AccountPopover />
         </Stack>
       </Toolbar>

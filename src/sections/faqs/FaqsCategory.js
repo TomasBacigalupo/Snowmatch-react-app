2c656b2719339a9075a594f825f1ebfe1e5a35d4
tbacigalupo <tbacigalupo@itba.edu.ar>
2023-02-15 09:32:00 -0300

    feature (FAQS): added faqs from snowmatch

--- src/sections/faqs/FaqsCategory.js
+++ src/sections/faqs/FaqsCategory.js
@@ -14,35 +14,40 @@ import { MotionViewport, varFade } from '../../components/animate';
 
 const CATEGORIES = [
   {
-    label: 'Managing your account',
+    label: 'Pros',
     icon: 'https://minimal-assets-api.vercel.app/assets/icons/faqs/ic_account.svg',
     href: '#',
   },
   {
-    label: 'Payment',
-    icon: 'https://minimal-assets-api.vercel.app/assets/icons/faqs/ic_payment.svg',
+    label: 'Guests',
+    icon: 'https://minimal-assets-api.vercel.app/assets/icons/faqs/ic_account.svg',
     href: '#',
-  },
-  {
-    label: 'Delivery',
-    icon: 'https://minimal-assets-api.vercel.app/assets/icons/faqs/ic_delivery.svg',
-    href: '#',
-  },
-  {
-    label: 'Problem with the Product',
-    icon: 'https://minimal-assets-api.vercel.app/assets/icons/faqs/ic_package.svg',
-    href: '#',
-  },
-  {
-    label: 'Return & Refund',
-    icon: 'https://minimal-assets-api.vercel.app/assets/icons/faqs/ic_refund.svg',
-    href: '#',
-  },
-  {
-    label: 'Guarantees and assurances',
-    icon: 'https://minimal-assets-api.vercel.app/assets/icons/faqs/ic_assurances.svg',
-    href: '#',
-  },
+  }
+  // {
+  //   label: 'Payment',
+  //   icon: 'https://minimal-assets-api.vercel.app/assets/icons/faqs/ic_payment.svg',
+  //   href: '#',
+  // },
+  // {
+  //   label: 'Delivery',
+  //   icon: 'https://minimal-assets-api.vercel.app/assets/icons/faqs/ic_delivery.svg',
+  //   href: '#',
+  // },
+  // {
+  //   label: 'Problem with the Product',
+  //   icon: 'https://minimal-assets-api.vercel.app/assets/icons/faqs/ic_package.svg',
+  //   href: '#',
+  // },
+  // {
+  //   label: 'Return & Refund',
+  //   icon: 'https://minimal-assets-api.vercel.app/assets/icons/faqs/ic_refund.svg',
+  //   href: '#',
+  // },
+  // {
+  //   label: 'Guarantees and assurances',
+  //   icon: 'https://minimal-assets-api.vercel.app/assets/icons/faqs/ic_assurances.svg',
+  //   href: '#',
+  // },
 ];
 
 // ----------------------------------------------------------------------

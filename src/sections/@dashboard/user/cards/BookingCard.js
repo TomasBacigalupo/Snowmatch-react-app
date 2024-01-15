b64d823f33cce6aa37b448161758b7ccea8fad79
Tomas Bacigalupo <tbacigalupo@itba.edu.ar>
2024-01-15 15:10:23 -0300

    more details

--- src/sections/@dashboard/user/cards/BookingCard.js
+++ src/sections/@dashboard/user/cards/BookingCard.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import { useState } from "react";
-import { Typography, Box, Card, Button, Avatar } from "@mui/material";
+import { Typography, Box, Card, Button, Avatar, Divider } from "@mui/material";
 import Iconify from "src/components/Iconify";
 import { formatDate } from "@fullcalendar/react";
 import { useNavigate } from "react-router";
@@ -72,7 +72,7 @@ export default function BookingCard({ booking, showInfo = true }) {
     return (
         <>
             <Card
-                onClick={()=> setOpen(true)}
+                onClick={() => setOpen(true)}
                 sx={{ display: 'flex', justifyContent: 'space-between', p: 2 }}>
                 <Box >
                     <Typography variant="body2" sx={{ color: 'text.secondary' }}>
@@ -96,35 +96,120 @@ export default function BookingCard({ booking, showInfo = true }) {
                     '& .MuiDrawer-paper': {
                         boxSizing: 'border-box', width: '100%', paddingBottom: 2, borderTopLeftRadius: '12px',  // Adjust the value as needed
                         borderTopRightRadius: '12px',
-                        height: '90%',
-                        marginTop: '20px'
+                        height: '95%',
+                        marginTop: '20px',
+                        paddingX: 1
                     }
                 }}
             >
                 <Box mt={1}>
                     <IconButton onClick={() => setOpen(false)}>
-                        <Iconify icon={'ic:round-arrow-back-ios'} width={20} height={20} />
+                        <Iconify icon={'line-md:close-circle'} width={25} height={25} />
                     </IconButton>
                 </Box>
-                <Grid p={2} spacing={3}>
+                <Grid container p={2} spacing={3}>
                     <Grid item xs={12}>
-                        <Typography variant="h3" gutterBottom>
+                        <Typography variant="h2" gutterBottom>
                             Detalles de la Reserva
                         </Typography>
                     </Grid>
                     <Grid item xs={12}>
-                        <Typography variant="h6" gutterBottom>
-                            Estricta
+                        <Divider sx={{ pt: 2 }} />
+                    </Grid>
+                    <Grid item xs={12} >
+                        <Box
+                            sx={{ display: 'flex', justifyContent: 'space-between', p: 2 }}>
+                            <Box >
+                                <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                                    {booking.state}
+                                </Typography>
+                                <Typography variant="h5" sx={{ mb: 0.5 }}>
+                                    {isStudent ? booking?.teacher?.name : booking?.student?.name}
+                                </Typography>
+                                <Typography variant="h5" sx={{ color: 'text.secondary' }}>
+                                    {formatDate(booking.eventList[0].start, { month: 'short', day: 'numeric' })} - {formatDate(booking.eventList[0].end, { month: new Date(booking.eventList[0].end).getMonth() === new Date(booking.eventList[0].start).getMonth() ? undefined : 'short', day: 'numeric' })}
+                                </Typography>
+                            </Box>
+                            <Avatar alt={isStudent ? booking?.teacher?.name : booking?.student?.name} src={'avatarUrl'} sx={{ width: 40, height: 40 }} />
+                        </Box>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Divider sx={{ borderBottomWidth: 8, }} />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Typography variant="h3" paragraph>
+                            Sobre {booking?.student?.name}
+                        </Typography>
+                        <Box display='flex' flexDirection='column'>
+                            <Box display='flex' flex={1} alignItems='center' mb={1}>
+                                <Iconify icon={'material-symbols-light:account-circle-outline'} width={40} height={40} />
+                                <Typography variant="body1" paragraph ml={2} mb={0}>
+                                    {`${booking?.student?.name} ${booking?.student?.lastname}`}
+                                </Typography>
+                            </Box>
+                            <Box display='flex' flex={1} alignItems='center'>
+                                <Iconify icon={'material-symbols-light:verified-user-outline'} width={40} height={40} />
+                                <Typography variant="body1" paragraph ml={2} mb={0} alignItems='center'>
+                                    Verificado
+                                </Typography>
+                            </Box>
+                            <Button variant="outlined" sx={{ mt: 2, py: 1 }} onClick={() => navigate(PATH_GUEST + '/' + booking?.student?.id)}>
+                                Mensaje
+                            </Button>
+                            <Button variant="outlined" sx={{ mt: 2, py: 1 }} onClick={() => navigate(PATH_DASHBOARD.user.profile)}>
+                                Llamar
+                            </Button>
+                        </Box>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Divider sx={{ borderBottomWidth: 8, }} />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Typography variant="h3" paragraph>
+                            Detalles de la reserva
+                        </Typography>
+                        <Box mb={2}>
+                            <Typography sx={{fontSize: '20px'}}>
+                                {`Cantidad de días`}
+                            </Typography>
+                            <Typography variant="body1" paragraph>
+                                {`${booking?.eventList.length} días`}
+                            </Typography>
+                            <Divider />
+                        </Box>
+                        <Box mb={2}>
+                            <Typography sx={{fontSize: '20px'}}>
+                                {`Horarios`}
+                            </Typography>
+                            <Typography variant="body1" paragraph>
+                                {`${booking?.eventList.length} días`}
+                            </Typography>
+                            <Divider />
+                        </Box>
+                        <Box>
+                            <Typography sx={{fontSize: '20px'}}>
+                                {`Alumnos`}
+                            </Typography>
+                            <Typography variant="body1" paragraph>
+                                {`${booking?.eventList.length} alumnos`}
+                            </Typography>
+                            <Divider />
+                        </Box>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Divider sx={{ borderBottomWidth: 8, }} />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Typography variant="h3" paragraph>
+                            Detalles del pago
                         </Typography>
                     </Grid>
                     <Grid item xs={12}>
-                        <Typography variant="body1" paragraph>
-                            Este instructor tiene un política de cancelación estricta. Si cancelas tu reserva con menos de 15 días de anticipación, no se hará reembolso.
-                        </Typography>
+                        <Divider sx={{ borderBottomWidth: 8, }} />
                     </Grid>
                     <Grid item xs={12}>
-                        <Typography variant="body1" paragraph>
-                            En caso de cancelar con mas de 15 dias de anticipación, se hará un reembolso del 50%.
+                        <Typography variant="h3" paragraph>
+                            Soporte
                         </Typography>
                     </Grid>
                 </Grid>
